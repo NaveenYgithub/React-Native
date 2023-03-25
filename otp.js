9294547facb20d90a1c8f6322766ad99f86e191e
NaveenYgithub <128901478+NaveenYgithub@users.noreply.github.com>
2023-03-25 16:42:11 +0530

    Add files via upload

---
+++ otp.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import './otp.css'
+
+
+function Otp (){
+   const [value,setValue]=useState("")
+   const navigate=useNavigate()
+       
+       const handleValue=(e)=>{
+            setValue(e.target.value)
+        }
+      const Submit=()=>{
+        if(value != 9999){
+           alert("invalid OTP")
+        }
+        else if(value ==""){
+            alert("invalid OTP")
+        }
+        else{ 
+            navigate("/details")
+        }
+      }   
+   
+        return (
+            <div><br/><br/>
+                <center>
+                    <div id="div">
+                        <div id="left">
+                            <form onSubmit={Submit} >
+                            <h1>Enter OTP</h1><br/>
+                            <h4>We have sent a one time password (OTP) to **********. </h4><br/><br/>
+                            <input placeholder="OTP" value={value} onChange={handleValue}></input>
+                            <button id="button"> -&gt;</button>
+                            </form>
+                            <h4>Resend OTP in 00:15</h4>
+                        </div>
+                    </div>
+                </center>
+            </div>
+        )
+        }
+
+export default Otp
